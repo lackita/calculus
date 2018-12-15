017a5218c85c07e4154a2945cc82e34217b125c3
Colin Williams <lackita@gmail.com>
2018-12-15 13:16:10 -0500

    Routing between even function and finished
    Not the right algorithm when I get there, but have the study url
    redirect to the next thing to study. I thought about just rendering
    everything at /study, but concluded that if I ever wanted to pass a
    link to a problem having that information in the url would be useful.
    
    I also learned the difference between a 301 and 303 redirect. I
    started with a 301 and then spent a bunch of time scratching my head
    as to why the redirect wasn't changing. Since it's considered to be
    moved permanently, browsers take that as license to cache the
    subsequent location and send you directly there.

--- build.boot
+++ build.boot
@@ -5,4 +5,5 @@
                  [io.pedestal/pedestal.jetty "0.5.1"]
                  [org.clojure/data.json "0.2.6"]
                  [org.slf4j/slf4j-simple "1.7.21"]
-                 [com.taoensso/carmine "2.19.1"]])
+                 [com.taoensso/carmine "2.19.1"]
+                 [hiccup "1.0.5"]])
--- src/main.clj
+++ src/main.clj
@@ -1,13 +1,44 @@
 (ns main
   (:require [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]))
+            [io.pedestal.http.route :as route]
+            [taoensso.carmine :as car :refer (wcar)]
+            [hiccup.core :refer (html)]))
 
 (defonce server (atom nil))
+(def redis-conn {:pool {} :spec {:uri "redis://localhost:6379/"}})
+(defmacro wcar* [& body] `(car/wcar redis-conn ~@body))
+
+(defn ok [body]
+  {:status 200
+   :body (html [:html
+                [:head [:script {:src "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML" :async :async}]]
+                [:body body]])
+   :headers {"Content-Type" "text/html"}})
+
+(defn view-even-function [_]
+  (wcar* (car/set "continue" "false"))
+  (ok [:b "even problem"]))
+
+(defn view-finished [_]
+  (wcar* (car/set "continue" "true"))
+  (ok "Finished Studying"))
+
+(defn redirect [route]
+  {:status 303 :headers {"Location" (route/url-for route)}})
+
+(defn study-redirect [_]
+  (if (= (wcar* (car/get "continue")) "true")
+    (redirect :even-function)
+    (redirect :finished)))
+
+(def routes
+  (route/expand-routes
+   #{["/study" :get study-redirect :route-name :study-query]
+     ["/study/even-function" :get view-even-function :route-name :even-function]
+     ["/study/finished" :get view-finished :route-name :finished]}))
 
 (def service-map
-  {::http/routes (route/expand-routes
-                  #{["/study" :get (fn [_] {:status 200 :body "Finished Studying"})
-                     :route-name :study-query]})
+  {::http/routes routes
    ::http/type :jetty
    ::http/port 8890})
 
